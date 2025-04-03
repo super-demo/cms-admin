c15da591c9046733fca199b712c95b2d33139d38
thyms-c <tat@appsynth.net>
2025-04-03 16:07:51 +0700

    hotfix: layout and remove unused comment

--- src/app/(control-panel)/workspaces-mini-apps/(workspaces)/[...workspace]/_components/workspace-thread.tsx
+++ src/app/(control-panel)/workspaces-mini-apps/(workspaces)/[...workspace]/_components/workspace-thread.tsx
@@ -18,50 +18,52 @@ export default function WorkspaceThread({
   const router = useRouter()
 
   return (
-    <div className="my-6 flex space-x-5">
-      {workspaceIdList.length < 3 && (
-        <CreateWorkspaceCard
-          handleClick={() =>
-            workspaceIdList.length === 1
-              ? router.push(
-                  pathWithSlug.CREATE_WORKSPACE_LV_1(workspaceIdList[0])
-                )
-              : workspaceIdList.length === 2
+    <div className="my-5 w-full px-1">
+      <div className="grid grid-cols-4 gap-5">
+        {workspaceIdList.length < 3 && (
+          <CreateWorkspaceCard
+            handleClick={() =>
+              workspaceIdList.length === 1
                 ? router.push(
-                    pathWithSlug.CREATE_WORKSPACE_LV_2(
-                      workspaceIdList[0],
-                      workspaceIdList[1]
-                    )
+                    pathWithSlug.CREATE_WORKSPACE_LV_1(workspaceIdList[0])
                   )
-                : workspaceIdList.length === 3
+                : workspaceIdList.length === 2
                   ? router.push(
-                      pathWithSlug.CREATE_WORKSPACE_LV_3(
+                      pathWithSlug.CREATE_WORKSPACE_LV_2(
                         workspaceIdList[0],
-                        workspaceIdList[1],
-                        workspaceIdList[2]
+                        workspaceIdList[1]
                       )
                     )
-                  : router.push(path.CREATE_WORKSPACE)
-          }
-        />
-      )}
+                  : workspaceIdList.length === 3
+                    ? router.push(
+                        pathWithSlug.CREATE_WORKSPACE_LV_3(
+                          workspaceIdList[0],
+                          workspaceIdList[1],
+                          workspaceIdList[2]
+                        )
+                      )
+                    : router.push(path.CREATE_WORKSPACE)
+            }
+          />
+        )}
 
-      {workspaces
-        ? workspaces.map((workspace) => (
-            <WorkspaceCard
-              key={workspace.site_id}
-              name={workspace.name}
-              short_description={workspace.short_description}
-              image_url={workspace.image_url}
-              parent_name={workspace.site_parent_name}
-              handleClick={() => {
-                router.push(
-                  `${path.WORKSPACES_MINI_APPS}/${workspaceIdList.join("/")}/${workspace.site_id}`
-                )
-              }}
-            />
-          ))
-        : ""}
+        {workspaces
+          ? workspaces.map((workspace) => (
+              <WorkspaceCard
+                key={workspace.site_id}
+                name={workspace.name}
+                short_description={workspace.short_description}
+                image_url={workspace.image_url}
+                parent_name={workspace.site_parent_name}
+                handleClick={() => {
+                  router.push(
+                    `${path.WORKSPACES_MINI_APPS}/${workspaceIdList.join("/")}/${workspace.site_id}`
+                  )
+                }}
+              />
+            ))
+          : ""}
+      </div>
     </div>
   )
 }
--- src/components/cards/workspace-card.tsx
+++ src/components/cards/workspace-card.tsx
@@ -18,13 +18,6 @@ export default function WorkspaceCard({
   image_url,
   handleClick
 }: WorkspaceCardProps) {
-  // const router = useRouter()
-
-  // const handleClick = (site_id: string) => {
-  //   // const formattedUrl = url.replace(/\s+/g, "-").toLowerCase()
-  //   router.push(pathWithSlug.WORKSPACE_SLUG(site_id.toLocaleString()))
-  // }
-
   return (
     <div>
       <div className="hover-card h-[268px] w-[236px] rounded-2xl border border-gray-200 p-6">
@@ -44,33 +37,12 @@ export default function WorkspaceCard({
             </Avatar>
           </div>
           <div className="flex flex-col items-center space-y-1 text-sm">
-            <h1 className="text-base font-bold">{name}</h1>
+            <h1 className="text-center text-base font-bold">{name}</h1>
             <h1>{short_description ?? "-"}</h1>
           </div>
           <Badge>{parent_name}</Badge>
         </div>
       </div>
-
-      {/* <div className="hover-card w-[236px] rounded-2xl border border-gray-200 p-6">
-        <div className="hover-card-content flex w-full flex-col items-center space-y-4">
-          <div className="w-full text-end text-xs text-neutral-500">2nd</div>
-          <div className="flex items-center justify-center">
-            <Avatar>
-              <AvatarImage
-                src="https://github.com/thyms.png"
-                alt="workspace_icon"
-                className="size-16 rounded-full"
-              />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-          </div>
-          <div className="flex flex-col items-center text-sm">
-            <h1 className="text-base font-bold">สำนักคอมพิวเตอร์</h1>
-            <h1>สำนักคอมพิวเตอร์ บางเขน</h1>
-          </div>
-          <Badge>Kasetsart Bangkhen</Badge>
-        </div>
-      </div> */}
     </div>
   )
 }
